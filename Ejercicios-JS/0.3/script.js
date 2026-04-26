95a5788e109b71c43a52676da446506c49a48c2d
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2026-04-26 00:03:15 -0300

    Practica

--- Ejercicios-JS/0.3/script.js
+++ Ejercicios-JS/0.3/script.js
@@ -1,40 +1,42 @@
+// https://pokeapi.co/api/v2/pokemon/${pokemon}
 const input = document.querySelector('#buscar');
 const boton = document.querySelector('#boton');
 const nombre = document.querySelector('#nombre');
 const img = document.querySelector('#imagen');
 const lista = document.querySelector('#lista');
 
-boton.addEventListener ('click', ()=>{
-    if(input.value!==''){
-        Render(input.value)
-        input.value = '';
-    }
-})
-
-const Render = async (pokemon) => {
-    nombre.innerHTML = 'Cargando...';
+const cargarDatos = (pokemon) => {
     lista.innerHTML = '';
-    img.src = '';
-    try {
-        const datos = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-        if (datos.ok){
-            const json = await datos.json();
-            nombre.innerHTML = json.name;
-            img.src = json.sprites.front_default;
-            habilidades(json);
-        }else{
-            nombre.innerHTML = 'No existe ese Pokemón';
-        }
-    } catch (error) {
-        nombre.innerHTML = 'Se produjo un error';
-    }
-    
-}
-
-const habilidades = (json) => {
-    json.abilities.forEach(element => {
+    nombre.innerHTML = pokemon.name;
+    img.src = pokemon.sprites.front_default;
+    pokemon.abilities.forEach(element => {
         const li = document.createElement('li');
-        li.textContent= element.ability.name;
+        console.log(element);
+        li.innerHTML = element.ability.name;
         lista.appendChild(li);
     });
 }
+
+const buscar = async () =>{
+    lista.innerHTML = 'Cargando...';
+    nombre.innerHTML = '';
+    img.src = '';
+    try {
+        const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${input.value}`);
+        if (data.ok){
+            const json = await data.json();
+            nombre.innerHTML = json.name;
+            cargarDatos(json);
+        }else{
+            lista.innerHTML = 'No existe el pokemon';
+        }
+    } catch (error) {
+        lista.innerHTML = error;
+    }
+}
+
+boton.addEventListener('click',()=>{
+    if (input!=''){
+        buscar();
+    }
+})
