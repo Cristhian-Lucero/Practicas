146f398f3b9088f68051aaf17cedbb8765e51dcd
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2026-04-30 21:11:35 -0300

    Tic Tac Toe

--- React/projects/vite-project/src/App.jsx
+++ React/projects/vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import { TwitterFollowCard } from "./TwitterFollowCard"
 export default function App () {
   return (
     <>
-      <TwitterFollowCard inicialIsFollowing nombre="Pepe" arroba="Pepe" imagen="https://cdn-icons-png.flaticon.com/128/1903/1903496.png" />
+      <TwitterFollowCard inicialIsFollowing nombre="Tedro" arroba="Tedro" imagen="https://cdn-icons-png.flaticon.com/128/1903/1903496.png" />
       <TwitterFollowCard nombre="Pepe" arroba="Pepe" imagen="https://cdn-icons-png.flaticon.com/128/6062/6062646.png" />
     </>
   )
