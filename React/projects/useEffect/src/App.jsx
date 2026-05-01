7ba2205a805df700c384a5c3c70eac65f021820b
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2026-05-01 14:07:50 -0300

    LocalStorage

--- React/projects/useEffect/src/App.jsx
+++ React/projects/useEffect/src/App.jsx
@@ -7,30 +7,43 @@ import { Winner } from './components/Winner';
 import { Tablero } from './components/Tablero';
 
 function App() {
-  const [tablero,setTablero] = useState(Array(9).fill(null))
-  const [turno,setTurno] = useState(Turnos.X)
+  const [tablero,setTablero] = useState(() => {
+    const estadoTablero = JSON.parse(localStorage.getItem('estadoTablero'))
+    return estadoTablero ? estadoTablero : Array(9).fill(null)
+  })
+  const [turno,setTurno] = useState(() => {
+    const estadoTurno = localStorage.getItem('estadoTurno')
+    return estadoTurno ? estadoTurno : Turnos.X
+  })
   const [winner,setWinner] = useState(null) //null: no hay ganador, false: hay empate
 
   const updateTablero = (indice) => {
     if (tablero[indice] || winner) return
-
+    // Cambia el turno
     const newTurno = (turno===Turnos.X) ? Turnos.O : Turnos.X
+    setTurno(newTurno)
+    // Actualiza el Tablero
     const newTablero = [...tablero]
     newTablero[indice] = turno
     setTablero(newTablero)
+    // guardar estado 
+    localStorage.setItem('estadoTablero', JSON.stringify(newTablero))
+    localStorage.setItem('estadoTurno', newTurno)
+    // revisda si hay ganador
     const newWinner = checkWinner(newTablero)
     if (newWinner) {
       setWinner(newWinner)
     } else if (checkEndGame(newTablero)){
-      setWinner(false)
+      setWinner(false) // Empate
     }
-    setTurno(newTurno)
   }
 
   const resetGame = () => {
     setTablero(Array(9).fill(null))
     setTurno(Turnos.X)
     setWinner(null)
+    localStorage.removeItem('estadoTablero')
+    localStorage.removeItem('estadoTurno')
   }
 
   return (
