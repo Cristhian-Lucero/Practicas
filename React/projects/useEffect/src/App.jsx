a9b1fa1723aca41961d7cdb8269256f7619398de
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2026-05-01 15:44:44 -0300

    fix

---
+++ React/projects/useEffect/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>UseEffect</h1>
+    </>
+  )
+}
+
+export default App
--- React/projects/useState/src/App.jsx
+++ React/projects/useState/src/App.jsx
@@ -1,43 +1,43 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 import { Relleno } from './components/Relleno';
-import { Turnos, WINNER_COMBOS } from './constanst';
+import { Turnos } from './constanst';
 import { checkWinner, checkEndGame } from './logic/tablero';
 import { Winner } from './components/Winner';
 import { Tablero } from './components/Tablero';
 import { saveGameToStorage, resetGameToStorage } from './storage';
 
 function App() {
-  // useState
-  const [tablero,setTablero] = useState(() => {
+  // useState 
+  const [tablero, setTablero] = useState(() => {
     const saved = localStorage.getItem('estadoTablero')
     if (!saved) return Array(9).fill(null)
     try { return JSON.parse(saved) ?? Array(9).fill(null) }
     catch { return Array(9).fill(null) }
   })
-  const [turno,setTurno] = useState(() => {
+  const [turno, setTurno] = useState(() => {
     const estadoTurno = localStorage.getItem('estadoTurno')
     return estadoTurno ? estadoTurno : Turnos.X
   })
-  const [winner,setWinner] = useState(null) //null: no hay ganador, false: hay empate
+  const [winner, setWinner] = useState(null) //null: no hay ganador, false: hay empate
 
 
   const updateTablero = (indice) => {
     if (tablero[indice] || winner) return
     // Cambia el turno
-    const newTurno = (turno===Turnos.X) ? Turnos.O : Turnos.X
+    const newTurno = (turno === Turnos.X) ? Turnos.O : Turnos.X
     setTurno(newTurno)
     // Actualiza el Tablero
     const newTablero = [...tablero]
     newTablero[indice] = turno
     setTablero(newTablero)
     // guardar estado 
-    saveGameToStorage({newTablero:newTablero,newTurno:newTurno})
+    saveGameToStorage({ newTablero: newTablero, newTurno: newTurno })
     // revisda si hay ganador
     const newWinner = checkWinner(newTablero)
     if (newWinner) {
       setWinner(newWinner)
-    } else if (checkEndGame(newTablero)){
+    } else if (checkEndGame(newTablero)) {
       setWinner(false) // Empate
     }
   }
@@ -53,7 +53,7 @@ function App() {
     <>
       <h1>TIC TAC TOE</h1>
       <section className='tablero'>
-      <Tablero tablero={tablero} updateTablero={updateTablero}/>
+        <Tablero tablero={tablero} updateTablero={updateTablero} />
       </section>
       <section className='turno'>
         <Relleno isSelected={turno === Turnos.X}>{Turnos.X}</Relleno>
