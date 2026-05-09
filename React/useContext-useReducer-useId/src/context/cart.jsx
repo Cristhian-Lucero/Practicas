f88a412e61f7123001871ab30599c5953294005a
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2026-05-09 00:17:27 -0300

    fragmentar

--- React/useContext-useReducer-useId/src/context/cart.jsx
+++ React/useContext-useReducer-useId/src/context/cart.jsx
@@ -1,56 +1,15 @@
-import { createContext, useReducer, useState } from 'react'
+import { createContext } from 'react'
+import { cartReducer, cartInitialState } from '../reducers/cart'
+import { useCartReducer } from '../hooks/useCartReducer'
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const CartContext = createContext()
-const initialState = []
-const reducer = (state, action) => {
-  // type = QUÉ operación. payload = CON QUÉ datos.
-  const { type, payload } = action
-  switch (type) {
-    case 'ADD_TO_CART': {
-      const { id } = payload
-      const productInCartIndex = state.findIndex((item) => item.id === id)
-      if (productInCartIndex >= 0) {
-        const newState = structuredClone(state)
-        newState[productInCartIndex].quantity += 1
-        return newState
-      } else {
-        return [
-          ...state,
-          {
-            ...payload,
-            quantity: 1,
-          },
-        ]
-      }
-    }
-    case 'REMOVE_FROM_CART': {
-      const { id } = payload
-      return state.filter((item) => item.id !== id)
-    }
-    case 'CLEAR_CART':
-      return initialState
-  }
-  return state
-}
 
 export function CartProvier({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  const addToCart = (product) =>
-    dispatch({
-      type: 'ADD_TO_CART',
-      payload: product,
-    })
-  const removeFromCart = (product) =>
-    dispatch({
-      type: 'REMOVE_FROM_CART',
-      payload: product,
-    })
-  const clearCart = () =>
-    dispatch({
-      type: 'CLEAR_CART',
-    })
+  const { state, addToCart, removeFromCart, clearCart } = useCartReducer(
+    cartReducer,
+    cartInitialState,
+  )
 
   return (
     <CartContext.Provider
