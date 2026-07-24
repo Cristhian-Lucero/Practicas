deb6ef47c40fb325be09c56fb6f2ac45b6ff5cf3
Cristhian-Lucero <cristhianlucero1@gmail.com>
2026-07-24 18:59:25 -0300

    Winston

--- NodeJS/02-Bases/src/app.js
+++ NodeJS/02-Bases/src/app.js
@@ -7,5 +7,14 @@ const obj = { name: 'Jhon', birthdate: '2001-03-12' }
 const jhon = makePerson(obj)
 console.log({ jhon })
 */
+
+/*
+06 
 const { getPokemonById } = require('./js-foundation/06-promise')
 getPokemonById(1).then((pokemon) => console.log(pokemon))
+*/
+
+const { buildLogger } = require('./plugins')
+const logger = buildLogger('app.js')
+logger.log('Hola Mundo')
+logger.error('Paso algo malo')
--- NodeJS/02-Bases/src/plugins/index.js
+++ NodeJS/02-Bases/src/plugins/index.js
@@ -1,8 +1,10 @@
 const { getAge } = require('./get-age.plugin')
 const { getID } = require('./get-id.plugin')
 const { http } = require('./http-client.plugin')
+const buildLogger = require('./logger.pluggin')
 module.exports = {
   http,
   getAge,
   getID,
+  buildLogger,
 }
