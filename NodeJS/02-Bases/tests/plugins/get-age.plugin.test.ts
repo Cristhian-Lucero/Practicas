dbfcaec046211c43f918da3288d921ea1d78c120
Cristhian-Lucero <cristhianlucero1@gmail.com>
2026-07-27 13:16:15 -0300

    in test geatAge using Spy

--- NodeJS/02-Bases/tests/plugins/get-age.plugin.test.ts
+++ NodeJS/02-Bases/tests/plugins/get-age.plugin.test.ts
@@ -12,4 +12,12 @@ describe('getAge', ()=>{
         const calculateAge = new Date().getFullYear() - new Date(birthdate).getFullYear();
         expect(age).toBe(calculateAge)
     })
+    test('should return 0 years', ()=>{
+        // usamos spyOn para espiar la función getFullYear y devolver un valor fijo 
+        // (basicamente le decimos que queremos que devuelva un valor fijo para que no dependa de la fecha actual)
+        const spy = jest.spyOn(Date.prototype, 'getFullYear').mockReturnValue(1995)
+        const birthdate = '1985-10-21'
+        const age = getAge(birthdate)
+        expect(age).toBe(0)
+    })
 })
