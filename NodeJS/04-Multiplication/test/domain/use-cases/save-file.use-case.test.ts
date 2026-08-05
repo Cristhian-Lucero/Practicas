7ba59c3ecd51eeb0f8f89cb50eeccd0766aa07ce
Cristhian-Lucero <cristhianlucero1@gmail.com>
2026-08-05 13:50:21 -0300

    SaveFileTest false cases

--- NodeJS/04-Multiplication/test/domain/use-cases/save-file.use-case.test.ts
+++ NodeJS/04-Multiplication/test/domain/use-cases/save-file.use-case.test.ts
@@ -1,8 +1,13 @@
-import { SaveFile } from "../../../src/domain/use-cases/save-file.use-case"
+import { SaveFile, Options } from '../../../src/domain/use-cases/save-file.use-case';
 import * as fs from 'fs';
 
 describe('src/domain/use-cases/save-file.use-case',() => {
-    afterEach(() => {
+    const Options = {
+        fileContent: 'custom content',
+        fileDestination:`custom-outputs/file-destination`,
+        fileName:`custom-table-name`
+    }
+    afterAll(() => {
         // limpiar aoutputs despues de cada prueba
         const outputsFolder = 'outputs'
         const customOutputsFolder = 'custom-outputs'
@@ -11,6 +16,7 @@ describe('src/domain/use-cases/save-file.use-case',() => {
         if (existOutputs) fs.rmSync(outputsFolder, {recursive: true})
         if (existCustomOutputs) fs.rmSync(customOutputsFolder, {recursive: true})
     })
+    
     test('should saveFile create SaveFile with default value',() => {
         const filePath = 'outputs/table.txt'
         const saveFile = new SaveFile()
@@ -27,11 +33,6 @@ describe('src/domain/use-cases/save-file.use-case',() => {
     })
     test('should saveFile create SaveFile with custom value',() => {
         const saveFile = new SaveFile()
-        const Options = {
-            fileContent: 'custom content',
-            fileDestination:`custom-outputs/file-destination`,
-            fileName:`custom-table-name`
-        }
         const filePath = `${Options.fileDestination}/${Options.fileName}.txt`
         const result = saveFile.execute(Options)
         expect(result).toBe(true)
@@ -41,5 +42,25 @@ describe('src/domain/use-cases/save-file.use-case',() => {
         expect(checkFile).toBe(true)
         expect(fileContent).toBe(Options.fileContent)
     })
-    
+    test('should return false if directory could not be created', () => {
+        const saveFile = new SaveFile()
+        // mockImplementation: Sobreescribe la funcionalidad de mkdirSync con esa funcion de error
+        const mkdirSpy = jest.spyOn(fs, 'mkdirSync').mockImplementation(
+            () => { throw new Error('This is a custom error message from testing')}
+        )
+        const result = saveFile.execute(Options)
+        expect(result).toBe(false)
+        // reiniciamos la funcionalidad del spy para que no interfiera en otros tests
+        mkdirSpy.mockRestore()
+    })
+    test('should return false if file could not be created', () => {
+        const saveFile = new SaveFile()
+        const writeFileSpy = jest.spyOn(fs, 'writeFileSync').mockImplementation(
+            () => { throw new Error('This is a custom writing error message from testing')}
+        )
+
+        const result = saveFile.execute({fileContent: 'hola'})
+        expect(result).toBe(false)
+        writeFileSpy.mockRestore()
+    })
 })
