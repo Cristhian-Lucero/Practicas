eaefe64b8a5880557a1820e75113ffb53c5ec026
Cristhian-Lucero <cristhianlucero1@gmail.com>
2026-08-10 11:28:57 -0300

    Cron - install and test

--- NodeJS/05-NOC/src/presentation/server.ts
+++ NodeJS/05-NOC/src/presentation/server.ts
@@ -1,5 +1,15 @@
+import { CronJob } from "cron"
+
 export class Server {
     public static start() {
         console.log('Server started...')
+        const job = new CronJob(
+            '*/2 * * * * *', // cronTime
+            () => {
+                const date = new Date()
+                console.log('2 second', date);
+            }
+        );
+        job.start()
     }
 }
