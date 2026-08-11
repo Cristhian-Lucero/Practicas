ea9e300cb0c7a9043537b6d71eb1717efba2fa1b
Cristhian-Lucero <cristhianlucero1@gmail.com>
2026-08-11 17:41:08 -0300

    Datasource and Repository

---
+++ NodeJS/05-NOC/src/domain/datasources/log.datasourse.ts
@@ -0,0 +1,7 @@
+import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+
+export abstract class LogDatasource {
+    // cualquier origen de datos debe implementar este método para guardar un log (log = Entrada de datos)
+    abstract saveLog(log: LogEntity): Promise<void>
+    abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]>
+}
